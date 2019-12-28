af0b50bfead0bb00d392a0db21a2a31804340aab
Eoin O'Brien <iameoinobrien@gmail.com>
2019-12-28 15:26:51 +0000

    add test to make sure creation of object from stream works

--- LuasAPI.NET.Tests/Models/RealTimeInfoXmlParsingTests.cs
+++ LuasAPI.NET.Tests/Models/RealTimeInfoXmlParsingTests.cs
@@ -37,5 +37,17 @@ namespace LuasAPI.NET.Tests.Models
 
 			Assert.Throws<InvalidOperationException>(() => RealTimeInfo.CreateFromStream(stream));
 		}
+
+
+		[Fact]
+		public void CreateFromStream_CorrectXmlSchema_ReturnsObject()
+		{
+			string xml = "<stopInfo created=\"2019-12-28T12:06:00\" stop=\"St. Stephen's Green\" stopAbv=\"STS\"><message>Green Line Service Disruption See News</message><direction name=\"Inbound\"><tram destination=\"See news for information\" dueMins=\"\" /></direction><direction name=\"Outbound\"><tram dueMins=\"6\" destination=\"Sandyford\" /></direction></stopInfo>";
+			Stream stream = ConvertStringToStream(xml);
+
+			RealTimeInfo realTimeInfo = RealTimeInfo.CreateFromStream(stream);
+
+			Assert.Equal(typeof(RealTimeInfo), realTimeInfo.GetType());
+		}
 	}
 }
